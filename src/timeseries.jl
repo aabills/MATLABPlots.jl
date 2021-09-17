9fb6f060d9f3d81c0da5f21fbd14f3da12e05bcf
Alec Bills <abills@andrew.cmu.edu>
2021-09-17 11:51:40 -0400

    add string plot timeseries -- patch

--- src/timeseries.jl
+++ src/timeseries.jl
@@ -40,3 +40,24 @@ function plot(jl_dateTimes::Array{DateTime},y,lineSpec;options=Dict())
     MATLAB.eval_string("plot(x,y"*",\"$lineSpec\""*optionsString*")")
 end
 
+"""
+plots a timeseries 
+"""
+function plot_ts(jl_dateTimes,y,lineSpec;options=Dict())
+    #Make everything a string
+    dateTimeStrings = string.(jl_dateTimes)
+    #Not sure if this line is really necessary, but change format to matlab format
+    dateTimeStrings = replace.(dateTimeStrings,"T"=>" ")
+    lengthDTStrings = length.(dateTimeStrings)
+    #Ensure Consistent formatting
+    locationsOfShortStrings = findall(lengthDTStrings.==19)
+    dateTimeStrings[locationsOfShortStrings].=dateTimeStrings[locationsOfShortStrings].*".000"
+    MATLAB.@mput dateTimeStrings
+    #Convert to MATLAB DateTime
+    MATLAB.eval_string("x = datetime(dateTimeStrings,\"InputFormat\",\"yyyy-MM-dd HH:mm:ss.SSS\")")
+    MATLAB.@mput y
+    #Plot!
+    optionsString = namevaluepairs(options)
+    MATLAB.eval_string("plot(x,y"*",\"$lineSpec\""*optionsString*")")
+end
+
