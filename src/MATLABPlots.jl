9c0de80aaeee2658ac923554a5cecf1a36fda422
Alec Bills <abills@andrew.cmu.edu>
2022-05-01 16:18:05 -0400

    export scatter

--- src/MATLABPlots.jl
+++ src/MATLABPlots.jl
@@ -25,7 +25,7 @@ include("utils.jl")
 
 #(1D and 2D) Plotting
 include("2DMatlabPlots.jl")
-export plot,semilogx,loglog,semilogy,stairs
+export plot,semilogx,loglog,semilogy,stairs,scatter
 
 #Timeseries
 using Dates
