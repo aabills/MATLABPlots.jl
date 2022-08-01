5a7487ca3ee2035f35a6462ad87f62fc018d434f
Alec Bills <abills@andrew.cmu.edu>
2022-08-01 11:54:46 -0400

    Add Area Plot

--- src/StatisticalMatlabPlotting.jl
+++ src/StatisticalMatlabPlotting.jl
@@ -4,3 +4,22 @@ function histogram(X;options=Dict())
     optionString = namevaluepairs(options)
     MATLAB.eval_string("histogram(X "*optionString*")")
 end
+
+#https://www.mathworks.com/help/matlab/ref/area.html
+function area(X,Y::Array;options=Dict())
+    MATLAB.@mput X
+    MATLAB.@mput Y
+    optionString = namevaluepairs(options)
+    MATLAB.eval_string("area(X,Y$optionString)")
+end
+
+#https://www.mathworks.com/help/matlab/ref/area.html
+#this one is scalar
+function area(X,baseline::Real;options=Dict())
+    MATLAB.@mput X
+    MATLAB.@mput baseline
+    optionString = namevaluepairs(options)
+    MATLAB.eval_string("area(X,Y$optionString)")
+end
+
+
